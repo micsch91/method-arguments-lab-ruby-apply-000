6829ea80ac6b54ff61ab0be946e1437210456c91
Michael Schneider <mic.sch91@gmail.com>
2020-03-11 15:10:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,3 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-
-end
 
 def introduction_with_language(language)
   puts "Hi, my name is Josh and I am learning to program in #{language}."
